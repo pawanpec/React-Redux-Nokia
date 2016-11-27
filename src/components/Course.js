eeebcb1a4fcafb2c831cb7902b531fad00b0def4
pawanpec <pawanpec@gmail.com>
2016-11-27 19:14:09 +0530

    course component

--- src/components/Course.js
+++ src/components/Course.js
@@ -6,7 +6,7 @@ class Course extends Component {
     render() {
         return (
             <div>
-            <h1>{this.props.title}</h1>
+            <h1>{this.props.course.title}..........</h1>
             </div>
           /*  <GridTile
         titlePosition="top"
